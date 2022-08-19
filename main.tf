d7ce99e27285f2b1b017e5d3476c04a264355cc1
Saishankar <82284490+jyothiadevops@users.noreply.github.com>
2022-08-19 20:18:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@
     subnet_id = aws_subnet.publicsubnets.id
     route_table_id = aws_route_table.PublicRT.id
  }
- Route table Association with Private Subnet's
+ # Route table Association with Private Subnet
  resource "aws_route_table_association" "PrivateRTassociation" {
     subnet_id = aws_subnet.privatesubnets.id
     route_table_id = aws_route_table.PrivateRT.id
